9eeae6e83f0c2d40fed1182dce1d6b887ce7ed13
skpl57 <kania.skpl57@gmail.com>
2026-05-31 22:46:38 +0200

    Wall climbing

--- Assets/Player_Movement.cs
+++ Assets/Player_Movement.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -9,87 +10,137 @@ public class Player_Movement : MonoBehaviour
     [SerializeField] private float _sprintModifier = 2f;
     [SerializeField] private float _crouchModifier = 0.5f;
     [SerializeField] private float _jumpHeight = 2f;
-    
-    private float _raycastLenght = 1.1f;
+    [SerializeField] private float _downDrag = 0.5f;
+    [SerializeField] private float _headHeight = 1.1f;
 
     [SerializeField] private float _mouseSensitivity = 0.1f;
+
     private Transform _cameraTransform;
+    private float _wallCheckDistance = 1f;
 
     private CharacterController _controller;
     private float _xRotation = 0f;
     private float _savedVelocityModifier = 1f;
+
     private Vector3 _velocity;
     private Vector3 _movement = Vector3.zero;
+
+    private Vector3 _boxPosition;
+    private Vector3 _boxHalfExtents;
+    private float _boxWidthModifier = 0.7f;
+    private float _boxSide;
+    private Camera _mainCamera;
+
+    private bool _canJump = true;
     void Start()
     {
         _controller = GetComponent<CharacterController>();
-        Camera mainCamera = GetComponentInChildren<Camera>();
-        _cameraTransform = mainCamera.transform;
+        _mainCamera = GetComponentInChildren<Camera>();
+        _cameraTransform = _mainCamera.transform;
         Cursor.lockState = CursorLockMode.Locked;
+
+        _boxSide = _controller.radius * _boxWidthModifier;
+        _boxHalfExtents = new Vector3(_boxSide, 0.02f, _boxSide);
     }
 
     void Update()
     {
-        if (!Physics.Raycast(transform.position, Vector3.up, _raycastLenght, LayerMask.GetMask("Default")))
+        _boxPosition = transform.position + new Vector3(0, _headHeight * transform.localScale.y, 0);
+        bool isCeilingAbove = Physics.CheckBox(_boxPosition, _boxHalfExtents, transform.rotation, LayerMask.GetMask("Default"));
+        _canJump = _controller.isGrounded;
+        CalculateWallClimbing();
+
+        if (!isCeilingAbove)
         {
             if (Keyboard.current.leftCtrlKey.isPressed)
             {
+                if (transform.localScale.y > 0.5f) _controller.Move(Vector3.down * _downDrag);
                 transform.localScale = new Vector3(1f, 0.5f, 1f);
-            } // Przytrzymanie Ctrl zmniejsza postać
-            else transform.localScale = new Vector3(1f, 1f, 1f);
+                _jumpHeight = 1f;
+            }
+            else
+            {
+                transform.localScale = new Vector3(1f, 1f, 1f);
+                _jumpHeight = 2f;
+            }
             if (_controller.isGrounded) _savedVelocityModifier = 1f;
-            if (Keyboard.current.spaceKey.isPressed && _controller.isGrounded) _velocity.y = Mathf.Sqrt(-_jumpHeight * _gravity * 1.5f); // Skok
+            if (Keyboard.current.spaceKey.isPressed && _canJump) _velocity.y = Mathf.Sqrt(-_jumpHeight * _gravity * 1.5f); // Skok
         }
-        //else _velocity.y = (0 - _velocity.y) / 2f;
 
-        Debug.Log(_velocity.y.ToString());
+        CalculateBasicMovement();
+        CameraMovement();
 
-        #region Camera Movement
-            Vector2 mouseDelta = Mouse.current.delta.ReadValue();
-
-             float mouseX = mouseDelta.x * _mouseSensitivity;
-             float mouseY = mouseDelta.y * _mouseSensitivity;
-
-             _xRotation -= mouseY;
-             _xRotation = Mathf.Clamp(_xRotation, -90f, 90f);
-             _cameraTransform.localRotation = Quaternion.Euler(_xRotation, 0f, 0f);
-            transform.Rotate(Vector3.up * mouseX);
-        #endregion
-
-        #region Basic WASD movement
-            float moveX = 0f;
-            float moveZ = 0f;
-            if (Keyboard.current.wKey.isPressed) moveZ = 1f;
-            if (Keyboard.current.sKey.isPressed) moveZ = -1f;
-            if (Keyboard.current.dKey.isPressed) moveX = 1f;
-            if (Keyboard.current.aKey.isPressed) moveX = -1f;
-            _movement = new Vector3(moveX, 0f, moveZ).normalized;
-        #endregion
-
-
-        if (moveZ != 1f && !_controller.isGrounded) _savedVelocityModifier = _crouchModifier;
-        if (Keyboard.current.leftShiftKey.isPressed && _controller.isGrounded) _savedVelocityModifier = _sprintModifier; // Przytrzymanie Shift zwiększa prędkość
-        if (Keyboard.current.leftCtrlKey.isPressed && _controller.isGrounded) _savedVelocityModifier = _crouchModifier;
-
-
-
-        _movement *= _savedVelocityModifier;
         Vector3 moveDirection = transform.TransformDirection(_movement);
         _controller.Move(moveDirection * _speedFloat * Time.deltaTime);
 
-
-        // --- 3. GRAWITACJA ---
         if (_controller.isGrounded) _velocity.y = -2f;
-        else if (!_controller.isGrounded) _velocity.y += _gravity * Time.deltaTime;
+        else if (!_canJump) _velocity.y += _gravity * Time.deltaTime;
         _controller.Move(_velocity * Time.deltaTime);
 
-        if (Physics.Raycast(transform.position, Vector3.up, _raycastLenght * transform.localScale.y, LayerMask.GetMask("Default"))) if (_velocity.y > 0) _velocity.y = (0 - _velocity.y);
+        if (isCeilingAbove && _velocity.y > 0) _velocity.y = 0f;
+    }
+
+    private void CameraMovement()
+    {
+        Vector2 mouseDelta = Mouse.current.delta.ReadValue();
+
+        float mouseX = mouseDelta.x * _mouseSensitivity;
+        float mouseY = mouseDelta.y * _mouseSensitivity;
+
+        _xRotation -= mouseY;
+        _xRotation = Mathf.Clamp(_xRotation, -90f, 90f);
+        _cameraTransform.localRotation = Quaternion.Euler(_xRotation, 0f, 0f);
+        transform.Rotate(Vector3.up * mouseX);
+
+        float fovSpeed = 8f;
+        float targetFOV = (_savedVelocityModifier == _sprintModifier) ? 70f : (_savedVelocityModifier == _crouchModifier) ? 50f : 60f;
+        _mainCamera.fieldOfView = Mathf.Lerp(_mainCamera.fieldOfView, targetFOV, Time.deltaTime * fovSpeed);
+    }
+    private void CalculateBasicMovement()
+    {
+        float moveX = 0f;
+        float moveZ = 0f;
+
+        if (Keyboard.current.wKey.isPressed) moveZ = 1f;
+        if (Keyboard.current.sKey.isPressed) moveZ = -1f;
+        if (Keyboard.current.dKey.isPressed) moveX = 1f;
+        if (Keyboard.current.aKey.isPressed) moveX = -1f;
+
+        if (moveZ != 1f && !_controller.isGrounded) _savedVelocityModifier = _crouchModifier;
+
+        if (Keyboard.current.leftShiftKey.isPressed && _controller.isGrounded) _savedVelocityModifier = _sprintModifier;
+        if (Keyboard.current.leftCtrlKey.isPressed && _controller.isGrounded) _savedVelocityModifier = _crouchModifier;
+
+        _movement = new Vector3(moveX, 0f, moveZ).normalized;
+        _movement *= _savedVelocityModifier;
+    }
+
+    private void CalculateWallClimbing()
+    {
+        Vector3 rayModifier = transform.forward * _wallCheckDistance * transform.localScale.y;
+        bool canWallClimb = Physics.Raycast(transform.position + Vector3.up / 2f, transform.forward, _wallCheckDistance * transform.localScale.y, LayerMask.GetMask("Default")) &&
+                             !Physics.Raycast(transform.position + Vector3.up * 1.5f, transform.forward, _wallCheckDistance * transform.localScale.y, LayerMask.GetMask("Default"));
+        if (canWallClimb)
+        {
+            _velocity.y = 0f;
+            _canJump = true;
+        }
     }
 
     void OnDrawGizmos()
     {
-        Gizmos.color = Color.red;
-        Gizmos.DrawLine(transform.position, transform.position + Vector3.up * _raycastLenght * transform.localScale.y);
-    }
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine(transform.position + Vector3.up / 2f, transform.position + (Vector3.up / 2f) + transform.forward * _wallCheckDistance * transform.localScale.y);
+        Gizmos.DrawLine(transform.position + Vector3.up * 1.5f, transform.position + Vector3.up * 1.5f + transform.forward * _wallCheckDistance * transform.localScale.y);
 
+        if (Physics.CheckBox(_boxPosition, _boxHalfExtents, transform.rotation, LayerMask.GetMask("Default")))
+            Gizmos.color = Color.red;
+        else
+            Gizmos.color = Color.green;
+
+        Matrix4x4 rotationMatrix = Matrix4x4.TRS(_boxPosition, transform.rotation, transform.localScale);
+        Gizmos.matrix = rotationMatrix;
+        Gizmos.DrawWireCube(Vector3.zero, _boxHalfExtents);
+        Gizmos.matrix = Matrix4x4.identity;
+    }
 }
